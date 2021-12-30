40a29b8d6a570e4015b99101f689180c52a122d3
Md.Shakil Sarder <76450631+Shakil0007@users.noreply.github.com>
2021-12-30 22:51:57 +0600

    update this file

--- vms/vms/asgi.py
+++ vms/vms/asgi.py
@@ -3,6 +3,8 @@ ASGI config for vms project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
+
+
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
